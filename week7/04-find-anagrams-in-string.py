1b1474c787f13090eb3062ccd75c595a10d5ab49
David Lerner <dmlerner@gmail.com>
2022-11-27 19:42:37 -0800

    7.5

---
+++ week7/04-find-anagrams-in-string.py
@@ -0,0 +1,64 @@
+# https://leetcode.com/problems/find-all-anagrams-in-a-string/
+# three thirteen - three thiryt one for potentially correct
+# three thirty seven - pass 26/61
+# four o clock:pass 33/61, take a walk (refactored to count things in p even when they're extra)
+# four oh one: I was lying, fix minor bug, passes.
+# TODO: try this beast again.
+
+from collections import Counter
+
+class LetterCounter:
+    def __init__(self, p):
+        self.p_counts = Counter(p)
+        self.counts = Counter()
+        self.need = set(self.p_counts.keys())
+        self.extra = 0
+
+    def add(self, x):
+        # print('add', self.need, x, self.n_need, self.extra)
+        if x not in self.p_counts:
+            self.extra += 1
+            return
+
+        self.counts[x] += 1
+        if self.counts[x] == self.p_counts[x]:
+            self.need.remove(x)
+        elif self.counts[x] > self.p_counts[x]:
+            self.extra += 1
+
+    def remove(self, x):
+        if x not in self.counts:
+            self.extra -= 1
+            return
+
+        self.counts[x] -= 1
+        if self.counts[x] < self.p_counts[x]:
+            self.need.add(x)
+        else:
+            self.extra -= 1
+
+def find(s, p):
+    # Given two strings s and p, return an array of all the start indices of p's anagrams in s.
+    # You may return the answer in any order.
+    lc = LetterCounter(p)
+    i = j = 0
+    indices = []
+    # invariant: s_counts contains the counts of s[i:j]
+    while i <= len(s):
+        # lc.add(s[i])
+        if not lc.need:
+            if lc.extra == 0:
+                indices.append(i)
+            lc.remove(s[i])
+            i += 1
+        else:
+            if j == len(s):
+                break
+            lc.add(s[j])
+            j += 1
+
+    return indices
+
+s = 'abab'
+p = 'ab'
+print(find(s, p))
---
+++ week7/05-minimum-height-trees.py
@@ -0,0 +1,43 @@
+# https://leetcode.com/problems/minimum-height-trees/
+# seven on nine
+# seven sixteen start programming
+# seven fourty passing
+
+from collections import defaultdict, deque
+
+def get_mht_roots(n, edges):
+    if n == 1:
+        return [0]
+    # no need for n...
+    neighbors_by_node = defaultdict(set)
+    for (n1, n2) in edges:
+        neighbors_by_node[n1].add(n2)
+        neighbors_by_node[n2].add(n1)
+
+
+    frontier = None
+    # print(frontier)
+    next_level = [n for (n, ns) in neighbors_by_node.items() if len(ns) == 1]
+    prev_level = []
+    while frontier or next_level:
+        if not frontier:
+            print(next_level)
+            prev_level = next_level
+            frontier = deque(next_level)
+            next_level = []
+
+        active = frontier.popleft()
+        # print(active, next_level, prev_level)
+        if not neighbors_by_node[active]:
+            return prev_level or next_level
+        neighbor = neighbors_by_node[active].pop()
+        neighbors_by_node[neighbor].remove(active)
+        if len(neighbors_by_node[neighbor]) == 1:
+            next_level.append(neighbor)
+        del neighbors_by_node[active]
+n = 6
+edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
+
+n=2
+edges=[[0,1]]
+get_mht_roots(n, edges)
